22a9a0bb1b593fde2f4725ea10cc3de69165b0d5
Harish N A <coffee.dev98@gmail.com>
2024-01-29 21:35:02 +0530

    basic setup

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const prod = process.env.NODE_ENV === "production";
+
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  mode: prod ? "production" : "development",
+  entry: "./src/index.tsx",
+  output: {
+    path: __dirname + "/dist/",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        resolve: {
+          extensions: [".ts", ".tsx", ".js", ".json"],
+        },
+        use: "ts-loader",
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader", "postcss-loader"],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  devtool: prod ? undefined : "source-map",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+};
